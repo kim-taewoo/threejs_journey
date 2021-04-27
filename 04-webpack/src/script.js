dab33af6d48a77b764bdf70c3d0f7c590b27bca2
kim-taewoo <acoustic0419@gmail.com>
2021-04-28 01:49:34 +0900

    finished 05. includes position, group

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -13,40 +13,66 @@ const sizes = {
 // Scene
 const scene = new THREE.Scene();
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: "#ff0000" }); 나 'red' 도 가능.
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// // Object
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// // const material = new THREE.MeshBasicMaterial({ color: "#ff0000" }); 나 'red' 도 가능.
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
 
-// Position
-// mesh.position.x = 0.7;
-// mesh.position.y = -0.6;
-// mesh.position.z = 1;
-mesh.position.set(0.7, -0.6, 1);
+// // Position
+// // mesh.position.x = 0.7;
+// // mesh.position.y = -0.6;
+// // mesh.position.z = 1;
+// mesh.position.set(0.7, -0.6, 1);
 
-// Scale
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.5;
-// mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 0.5);
+// // Scale
+// // mesh.scale.x = 2;
+// // mesh.scale.y = 0.5;
+// // mesh.scale.z = 0.5;
+// mesh.scale.set(2, 0.5, 0.5);
 
-// Rotation
-// Vector3 가 아니라 Euler 다.(오일러) 축의 방향을 중심으로 회전하는 모양을 생각.
-// 파이가 반 바퀴가 된다. (수학적인 이유는 모르겠음)
-// 회전하면 다른 축의 위치가 바뀌기 때문에 회전 순서가 중요하다.
-// 이렇게 회전 순서가 중요해지는 문제 때문에 오일러 보다 Quaternion 이 더 많이 쓰인다.
-// 다만 Quaternion 이 이해하기는 어려운 큰 개념이다...
-// mesh.rotation.y = 1;
-mesh.rotation.reorder("YXZ");
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+// // Rotation
+// // Vector3 가 아니라 Euler 다.(오일러) 축의 방향을 중심으로 회전하는 모양을 생각.
+// // 파이가 반 바퀴가 된다. (수학적인 이유는 모르겠음)
+// // 회전하면 다른 축의 위치가 바뀌기 때문에 회전 순서가 중요하다.
+// // 이렇게 회전 순서가 중요해지는 문제 때문에 오일러 보다 Quaternion 이 더 많이 쓰인다.
+// // 다만 Quaternion 이 이해하기는 어려운 큰 개념이다...
+// // mesh.rotation.y = 1;
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
 
 // 유니티처럼 position 은 Vector3 클래스와 관련있다.
 // position.length() 는 scene 의 중심점으로 부터 이 mesh 의 position 사이의 거리를 구한다.
 // mesh.position.normalize();
 // console.log(mesh.position.length());
+
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube3);
 
 // Axes Helper (화면상에 x,y,z 축을 표시해주는 헬퍼 객체. 첫번째 인자는 표시될 축의 길이)
 const axesHelper = new THREE.AxesHelper();
@@ -59,7 +85,7 @@ camera.position.z = 3;
 // camera.position.set(1, 1, 3);
 scene.add(camera);
 
-camera.lookAt(mesh.position);
+// camera.lookAt(mesh.position);
 
 // console.log(mesh.position.distanceTo(camera.position));
 
