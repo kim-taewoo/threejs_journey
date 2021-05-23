c6cd3bf32002871f3d26da5bb7bd7c0388c4e7e0
kim-taewoo <acoustic0419@gmail.com>
2021-05-24 02:48:13 +0900

    done particles

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -37,12 +37,15 @@ const particlesGeometry = new BufferGeometry();
 const count = 5000;
 
 const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
 
 for (let i = 0; i < count * 3; i++) {
   positions[i] = (Math.random() - 0.5) * 10;
+  colors[i] = Math.random();
 }
 
 particlesGeometry.setAttribute("position", new BufferAttribute(positions, 3));
+particlesGeometry.setAttribute("color", new BufferAttribute(colors, 3));
 
 // Material
 const particlesMaterial = new PointsMaterial({
@@ -54,7 +57,12 @@ particlesMaterial.color = new Color("#ff88cc"); // 이후에 값 바꿔줄 때, 
 // particlesMaterial.map = particleTexture;
 particlesMaterial.transparent = true;
 particlesMaterial.alphaMap = particleTexture;
-particlesMaterial.alphaTest = 0.001;
+// particlesMaterial.alphaTest = 0.001; // 괜찮은 해결책
+// particlesMaterial.depthTest = false; // 아예 뭐가 앞이고 뒤에 있는지 테스트를 안 하는 해결책. 근데 숨겨져야 되는 게 보이는 등의 문제가 있다.
+particlesMaterial.depthWrite = false; // depthBuffer 에 depthTest 작성 하지 않기.
+
+particlesMaterial.blending = THREE.AdditiveBlending; // 겹치는 영역 빛을 합쳐서 표현하는 블렌딩. (퍼포먼스 이슈 있을 수 있음)
+particlesMaterial.vertexColors = true;
 
 // Points
 const particles = new Points(particlesGeometry, particlesMaterial);
@@ -115,6 +123,18 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Update particles
+  //   particles.rotation.y = elapsedTime * 0.02;
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3;
+    const x = particlesGeometry.attributes.position.array[i3];
+    particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(
+      elapsedTime + x
+    );
+  }
+
+  particlesGeometry.attributes.position.needsUpdate = true;
 
   // Update controls
   controls.update();
